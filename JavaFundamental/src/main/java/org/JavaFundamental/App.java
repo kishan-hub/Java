3df551c53bc9f9dfcfa01f84543436972f08f689
Kishan <kumarkishan2991@gmail.com>
2023-06-28 23:38:47 +0530

    created the featured branch for thread

--- JavaFundamental/src/main/java/org/JavaFundamental/App.java
+++ JavaFundamental/src/main/java/org/JavaFundamental/App.java
@@ -8,6 +8,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       Thread javaThread =new Thread(()->{
+    	   System.out.println("welcome to java");
+       });
+       
+
+       Thread pythonThread =new Thread(()->{
+    	   System.out.println("Welcome to python ");
+       });
+       
+       Thread javascriptThread =new Thread(()->{
+         System.out.println("Welcome to javascript ");   	   
+       });
+       
+       Thread shellScript =new Thread(()->{
+    	   System.out.println("Welcome to shell Script");
+       });
+       
+       javaThread.start();
+       pythonThread.start();
+       javascriptThread.start();
+       shellScript.start();
+       
+       
     }
 }
